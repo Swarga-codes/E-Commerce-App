f0dac56577f7a7c2bfb5e0e3679afc6fcac252c8
Swarga-codes <sd3526@srmist.edu.in>
2023-08-12 22:03:33 +0530

    Added product model

---
+++ Backend/models/productModel.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose')
+const {ObjectID}=mongoose.Schema.Types
+const productSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    type:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    discountedPrice:{
+        type:Number
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    reviews:[
+        {postedBy:{type:ObjectID,ref:'USER'},comment:{type:String}}
+    ],
+
+})
+const PRODUCTS=mongoose.model('PRODUCTS',productSchema)
+module.exports=PRODUCTS
