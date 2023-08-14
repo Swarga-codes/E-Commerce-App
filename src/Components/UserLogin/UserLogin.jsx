a37aef7bd8c61d33e9a412e6d0d0538bf94b7f8d
Swarga-codes <sd3526@srmist.edu.in>
2023-08-15 03:21:21 +0530

    added user auth ui

--- src/Components/UserLogin/UserLogin.jsx
+++ src/Components/UserLogin/UserLogin.jsx
@@ -61,7 +61,7 @@ export default function UserLogin() {
                   type="button"
                   className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                 >
-                  Get started <ArrowRight className="ml-2" size={16} />
+                  Log In <ArrowRight className="ml-2" size={16} />
                 </button>
               </div>
               <p className="mt-2text-sm text-gray-600 ">
--- src/Components/UserRegsiter/UserSignUp.jsx
+++ src/Components/UserRegsiter/UserSignUp.jsx
@@ -38,6 +38,32 @@ export default function UserSignup() {
                 </div>
               </div>
               <div>
+                <label htmlFor="" className="text-base font-medium text-gray-900">
+                  {' '}
+                  Name{' '}
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="text"
+                    placeholder="Full Name"
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="" className="text-base font-medium text-gray-900">
+                  {' '}
+                  Phone Number{' '}
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="tel"
+                    placeholder="Phone Number"
+                  ></input>
+                </div>
+              </div>
+              <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="" className="text-base font-medium text-gray-900">
                     {' '}
