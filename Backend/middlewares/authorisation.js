65e0f9f88607668800e470f2c5e5ebb82b7e1cb7
Swarga-codes <sd3526@srmist.edu.in>
2023-08-21 19:33:51 +0530

    updated middleware function for expiry of token

--- Backend/middlewares/authorisation.js
+++ Backend/middlewares/authorisation.js
@@ -7,7 +7,10 @@ module.exports=async(req,res,next)=>{
     const token=authorization.replace("Bearer ","")
     if(!token) return res.status(404).json({error:'Token not found, login again'})
     const checkToken=await jwt.verify(token,process.env.SECRET_KEY)
-    if(!checkToken) return res.status(403).json({error:'Token expired or invalid'})
+    if(!checkToken) return res.status(403).json({error:'Token invalid'})
+    if (checkToken.exp < Date.now() / 1000) {
+        return res.status(401).json({error:'Token has expired,log in again.'});
+      }
     const {_id}=checkToken
     const userdata=await USER.findById(_id)
     if(userdata){
