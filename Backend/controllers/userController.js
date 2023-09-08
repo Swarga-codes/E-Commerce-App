70f9e50827f7c5836ca841194d65ea757c3632d8
Swarga-codes <sd3526@srmist.edu.in>
2023-09-09 03:18:27 +0530

    updated create order logic

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ORDER = require('../models/ordersModel')
 const PRODUCTS = require('../models/productModel')
 const USER=require('../models/userModel')
+const SELLER=require('../models/sellerModel')
 //------------Update User---------------//
 const updateUserData=async(req,res)=>{
 const {email,name,phoneNumber,profilePic,address}=req.body
@@ -35,15 +36,23 @@ return res.status(200).json({message:'User details updated successfully', userDa
 // ------------Place Order---------------//
 const createOrder=async(req,res)=>{
   const {orderItems,sellersID,orderAmount,orderType}=req.body
-  if(orderItems.length===0 || sellersID.length===0 || !orderAmount || !orderType){
+  if(orderItems?.length===0 || sellersID?.length===0 || !orderAmount || !orderType){
     return res.status(400).json({error:'Please include the required fields'})
   }
+  //Checking the order items
+  let checkAmt=0
   for(let i=0;i<orderItems.length;i++){
-    let currItem=await PRODUCTS.find({_id:orderItems[i]})
+    let currItem=await PRODUCTS.findOne({_id:orderItems[i]})
     if(!currItem) return res.status(404).json({error:'Item does not exist'})
-    if(!sellersID.includes(currItem.createdBy)) return res.status(404).json({error:'Seller not found for the product'})
-    if(currItem.quantity<=0) return res.status(422).json({error:'One or more Item(s) is out of stock!'})
+    if(!sellersID.includes(currItem?.createdBy)) return res.status(404).json({error:'Seller not found for the product'})
+    if(currItem?.quantity<=0) return res.status(422).json({error:'One or more Item(s) is out of stock!'})
+    checkAmt+=currItem?.price;
   }
+  //Check whether the price is fine or not
+  if(checkAmt===orderAmount) return res.status(400).json({error:'Order Amount inappropriate'})
+//Checking the sellers ID
+const checkSellerId=await SELLER.find({_id:{$in:sellersID}}).toArray()
+if(checkSellerId?.length!==sellersID?.length) return res.status(404).json({error:'One or more sellers not found!'})
 if(orderType==='Cash On Delivery'){
 const newOrder=await new ORDER({
   orderItems,
@@ -57,12 +66,21 @@ const orderPlaced=await newOrder.save()
 if(!orderPlaced){
   return res.status(500).json({error:'Could not process your order, try again'})
 }
+//Update product stock
 for(let i=0;i<orderItems.length;i++){
-  const currentProduct=await PRODUCTS.find({_id:orderItems[i]})
+  const currentProduct=await PRODUCTS.findOne({_id:orderItems[i]})
   currentProduct.quantity-=1
   const updateProductQuantity=await PRODUCTS.updateOne({_id:currentProduct._id},{$set:{quantity:currentProduct.quantity}},{new:true})
   if(!updateProductQuantity) return res.status(500).json({error:'Could not update the existing product data,try again'}) 
 }
+//Update sellers orders list
+for(let i=0;i<sellersID.length;i++){
+  const seller=await SELLER.updateOne({_id:sellersID[i]},{$push:{ordersList:orderPlaced._id}},{new:true})
+  if(!seller) return res.status(500).json({error:'Could not update seller orders list'})
+}
+//update users orders list
+const updateUserOrderListAndCart=await USER.updateOne({_id:req.user._id},{$push:{ordersList:orderPlaced._id,cartItems:[]}},{new:true})
+if(!updateUserOrderListAndCart) return res.status(500).json({error:'Could not update user orders list and cart'})
 return res.status(200).json({message:'Order Placed Successfully'})
 }
 if(orderType==='UPI'){
