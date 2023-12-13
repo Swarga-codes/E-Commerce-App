2c951324d7baed0f870976bd68f65b841846d602
Swarga-codes <sd3526@srmist.edu.in>
2023-12-13 18:28:45 +0530

    added seller order tracker api

--- Backend/controllers/sellerController.js
+++ Backend/controllers/sellerController.js
@@ -1,9 +1,13 @@
 const ORDER=require('../models/ordersModel')
 // ------------Get Order Details for current SELLER---------------//
 const getSellerOrders=async(req,res)=>{
-    const orders=await ORDER.find()
-    console.log(req.seller._id)
+    try{
+    const orders=await ORDER.find({sellersID:{$in:[req.seller._id]}}).populate('sellersID').populate('orderItems')
     return res.status(200).json(orders)
+    }
+    catch(err){
+        return res.status(500).json({error:'Internal server error'})
+    }
   }
 
 
