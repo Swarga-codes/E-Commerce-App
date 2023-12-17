39cdbe6f66ce10349653b4c36d9cb4567c15a90b
Swarga-codes <sd3526@srmist.edu.in>
2023-12-18 01:19:57 +0530

    added search bar functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,13 +11,17 @@ export default function Navbar() {
   const counter=useSelector(state=>state.cart)
 
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
  const location=useLocation()
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
+  
+ 
 if(layoutException.includes(location.pathname)) {
 return null
 }
+
   return (
     <div className="relative w-full bg-white">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
@@ -31,13 +35,36 @@ return null
           <span className="font-bold text-2xl">MyShop.</span>
         </div>
         </Link>
-       <div className="flex grow justify-center">
+       <div className="flex-col flex grow justify-center items-center">
+       
           <input
             className="flex h-10 w-[50%] rounded-md bg-gray-200 px-3 py-2 border-solid border-2 border-black text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
             type="text"
             placeholder="Search for products..."
-          ></input>
+            ></input>
+        
+            {/*{searchQuery && (
+            <div className='w-[250px] z-30 border-solid border-2 rounded-xl shadow-md p-2 bg-white mt-[9rem]'>
+            {getSearchResults(searchQuery).length>0? (
+              getSearchResults(searchQuery)?.map(item=>(
+    <div className='mt-4 flex'>
+      <img src={item?.image} alt="no prev" className='w-16 h-16'/>
+      <div className='ml-5'>
+      <h1 className='font-bold'>{item?.title}</h1>
+      <p className='font-semibold'>${item?.discountedPrice}</p>
+      </div>
+      </div>
+      ))
+            )
+            :
+      <p className='font-bold'>No results found.</p>
+
+            }
+          
+      </div>
+          )}*/}
         </div>
+      
         <div className='mr-5 flex'>
        
         {localStorage.getItem('user_token') &&
