37ab5a5e478f07fb4e2a07fa4524cf64be7893fe
Swarga-codes <sd3526@srmist.edu.in>
2023-12-19 00:31:44 +0530

    added loader for my products page

--- src/Components/MyProducts/MyProducts.jsx
+++ src/Components/MyProducts/MyProducts.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import SellerNavbar from "../SellerNavbar/SellerNavbar";
 import { ProductCard } from "./ProductCard";
-
+import { Link } from "react-router-dom";
+import EmptyProduct from '../../assets/empty_product.json'
+import Lottie from "lottie-react";
+import { Ring } from "@uiball/loaders";
 function MyProducts() {
   const [productData, setProductData] = useState([]);
+  const [loader,setLoader]=useState(true)
   const fetchMyProducts = async () => {
     const response = await fetch(
       `${import.meta.env.VITE_SERVER_API}api/products/myProducts`,
@@ -16,6 +20,7 @@ function MyProducts() {
     );
     const data = await response.json();
     setProductData(data);
+    setLoader(false)
   };
   useEffect(() => {
     fetchMyProducts();
@@ -25,6 +30,30 @@ function MyProducts() {
       <SellerNavbar />
       <div className="ml-80 p-6">
         <h1 className="text-3xl font-bold">My Products.</h1>
+        {productData.length===0 && !loader && 
+        <div className="flex flex-col justify-center items-center">
+        <Lottie
+        animationData={EmptyProduct}
+        loop={false}
+        autoplay
+        className="w-fit rounded-md object-cover"
+      />
+        <h1 className="text-2xl font-bold mt-10">Woah! it's empty here!</h1>
+        <Link to='/seller/products/create'><span className="underline">Would you like to create some?</span></Link>
+        </div>
+        }
+        {
+            loader && 
+            <div className="flex flex-col justify-center items-center mt-20">
+            <Ring 
+          size={80}
+          lineWeight={5}
+          speed={2} 
+          color="black" 
+         />
+            <h1 className="text-2xl font-bold mt-10">Hang On, Your Products are loading...</h1>
+            </div>
+        }
         {productData?.map((product) => (
           <ProductCard product={product} />
         ))}
